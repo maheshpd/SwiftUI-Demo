fc067384aef11d25be038804086d60ebe95b554a
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-01-16 01:10:32 +0530

    Initial Commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Mahesh Prasad on 16/01/20.
+//  Copyright © 2020 CreatesApps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
